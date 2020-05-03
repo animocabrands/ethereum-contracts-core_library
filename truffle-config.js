2ae1cb49e0ea8d9202c95246f202228aad15d74c
Nathan Sala <sala.nathan@gmail.com>
2020-05-03 11:22:17 +0800

    Migration to solc 0.6.6, added PauserRole and MinterRole

--- truffle-config.js
+++ truffle-config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     compilers: {
         solc: {
-            version: '0.6.2',
+            version: '0.6.6',
             settings: {
                 optimizer: {
                     enabled: true,
