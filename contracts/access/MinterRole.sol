b4ae2b06116ceb0260d908e62d0066358142cf27
Jan-Paul Azucena <jp@fuelpowered.com>
2020-06-23 14:06:23 -0700

    [ADDED] Natspec commenting for the access control contract modules

--- contracts/access/MinterRole.sol
+++ contracts/access/MinterRole.sol
@@ -4,32 +4,59 @@ pragma solidity 0.6.8;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+/**
+ * Contract module which allows derived contracts access control over token
+ * minting operations.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyMinter`, which can be applied to the minting functions of your contract.
+ * Those functions will only be accessible to accounts with the minter role
+ * once the modifer is put in place.
+ */
 contract MinterRole is AccessControl {
 
     event MinterAdded(address indexed account);
     event MinterRemoved(address indexed account);
 
-    constructor () internal {
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        emit MinterAdded(_msgSender());
-    }
-
+    /**
+     * Modifier to make a function callable only by accounts with the minter role.
+     */
     modifier onlyMinter() {
         require(isMinter(_msgSender()), "MinterRole: caller does not have the Minter role");
         _;
     }
 
+    /**
+     * Constructor.
+     */
+    constructor () internal {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        emit MinterAdded(_msgSender());
+    }
+
+    /**
+     * Validates whether or not the given account has been granted the minter role.
+     * @param account The account to validate.
+     * @return True if the account has been granted the minter role, false otherwise.
+     */
     function isMinter(address account) public view returns (bool) {
         require(account != address(0), "MinterRole: address zero cannot be minter");
         return hasRole(DEFAULT_ADMIN_ROLE, account);
     }
 
+    /**
+     * Grants the minter role to a non-minter.
+     * @param account The account to grant the minter role to.
+     */
     function addMinter(address account) public onlyMinter {
         require(!isMinter(account), "MinterRole: add an account already minter");
         grantRole(DEFAULT_ADMIN_ROLE, account);
         emit MinterAdded(account);
     }
 
+    /**
+     * Renounces the granted minter role.
+     */
     function renounceMinter() public {
         require(isMinter(_msgSender()), "MinterRole: renounce by a non-minter");
         renounceRole(DEFAULT_ADMIN_ROLE, _msgSender());
--- contracts/access/PauserRole.sol
+++ contracts/access/PauserRole.sol
@@ -4,32 +4,59 @@ pragma solidity 0.6.8;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+/**
+ * Contract module which allows derived contracts to implement a mechanism for
+ * pausing a contract.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyPauser`, which can be applied to the pausable functions of your
+ * contract. Those functions will only be accessible to accounts with the
+ * pauser role once the modifer is put in place.
+ */
 contract PauserRole is AccessControl {
 
     event PauserAdded(address indexed account);
     event PauserRemoved(address indexed account);
 
-    constructor () internal {
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        emit PauserAdded(_msgSender());
-    }
-
+    /**
+     * Modifier to make a function callable only by accounts with the pauser role.
+     */
     modifier onlyPauser() {
         require(isPauser(_msgSender()), "PauserRole: caller does not have the Pauser role");
         _;
     }
 
+    /**
+     * Constructor.
+     */
+    constructor () internal {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        emit PauserAdded(_msgSender());
+    }
+
+    /**
+     * Validates whether or not the given account has been granted the pauser role.
+     * @param account The account to validate.
+     * @return True if the account has been granted the pauser role, false otherwise.
+     */
     function isPauser(address account) public view returns (bool) {
         require(account != address(0), "PauserRole: address zero cannot be pauser");
         return hasRole(DEFAULT_ADMIN_ROLE, account);
     }
 
+    /**
+     * Grants the pauser role to a non-pauser.
+     * @param account The account to grant the pauser role to.
+     */
     function addPauser(address account) public onlyPauser {
         require(!isPauser(account), "PauserRole: add an account already pauser");
         grantRole(DEFAULT_ADMIN_ROLE, account);
         emit PauserAdded(account);
     }
 
+    /**
+     * Renounces the granted pauser role.
+     */
     function renouncePauser() public {
         require(isPauser(_msgSender()), "PauserRole: renounce by a non-pauser");
         renounceRole(DEFAULT_ADMIN_ROLE, _msgSender());
