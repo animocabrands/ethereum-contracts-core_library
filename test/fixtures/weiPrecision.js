2a7cc024a408f464c5eda15461d676aa0f12eb80
Jan-Paul Azucena <jp@fuelpowered.com>
2020-06-24 14:58:12 -0700

    [ADDED] Wei precision function for equality testing with an allowable percent deviation of the actual value from the expected value

--- test/fixtures/weiPrecision.js
+++ test/fixtures/weiPrecision.js
@@ -31,7 +31,33 @@ function shouldBeEqualWithProportionalPrecision(actual, expected, divisor = 1000
     );
 }
 
+/**
+ * Validates that `acutal` equals `expected` with a difference not exceeding `P`% of the expected value.
+ * Where 0 < `P` <= 100, and `P` = `N` * 10 ^ `M`
+ * @param {BN} actual the actual value, in WEI.
+ * @param {BN} expected the expected value, in WEI.
+ * @param {Number} significand `N` in the above formula. This must be an unsigned integer > 0.
+ * @param {Number} orderOfMagnitude `M` in the above formula. This must be an integer <= 2. The default value is 0.
+ */
+function shouldBeEqualWithPercentPrecision(actual, expected, significand, orderOfMagnitude = 0) {
+    // e.g. 0.5% max deviation of expected
+    //  abs(expected - actual) <= expected * 5 * (10 ^ -1) / 100
+    //  abs(expected - actual) <= expected * significand * (10 ^ orderOfMagnitude) / 100
+    //  abs(expected - actual) <= expected * significand * (10 ^ orderOfMagnitude) / 10 ^ 2
+    //  abs(expected - actual) <= expected * significand / 10 ^ (2 - orderOfMagnitude)
+    //  abs(expected - actual) * (10 ^ (2 - orderOfMagnitude)) <= significand * expected
+    const delta = expected.sub(actual).abs();
+    const exponent = new BN(2 - orderOfMagnitude);
+    const coefficient = new BN(10);
+    const lhs = delta.mul(coefficient.pow(exponent));
+    const rhs = expected.muln(significand);
+    lhs.should.be.bignumber.lte(rhs,
+        `${fromWei(actual)} differs from expected ${fromWei(expected)} (percent tolerance ${significand} x 10 ^ ${orderOfMagnitude})`
+    );
+}
+
 module.exports = {
     shouldBeEqualWithETHDecimalPrecision,
-    shouldBeEqualWithProportionalPrecision
+    shouldBeEqualWithProportionalPrecision,
+    shouldBeEqualWithPercentPrecision
 }
